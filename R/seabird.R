696e244076eeade265eabf515dbe5e5de0095cd9
Leo C Schalkwyk <Leonard.Schalkwyk@kcl.ac.uk>
2013-03-12 13:29:53 +0000

    replaced ROCR dependency with ROC
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/wateRmelon@74222 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/seabird.R
+++ R/seabird.R
@@ -3,12 +3,18 @@ function(pr, stop=1, X){ # sexdiff pvalue ROC AUC
                                     # pr : pvals from sextest
                                     # stop: fraction for partial AUC
                                     # X: logical vector-probe on X?
-   if(!library(ROCR, logical.return=TRUE, quietly=TRUE)){
-      stop('can\'t load ROCR package')
-   }
-   pr   <- prediction(1-pr , X)
-   unlist(performance(pr, "auc", fpr.stop=stop)@y.values)
+# ROCR version
+#    pr <- prediction(1 - pr, X)
+#    unlist(performance(pr, "auc", fpr.stop = stop)@y.values)
+# pROC version
+#    as.numeric(auc(formula=X~pr, data=NULL)) 
+# ROC version (bioconductor)
+
+   pAUC(rocdemo.sca(truth=X, data=1-pr), stop) 
+
 }
+   
+
 
 
 seabi <- 
