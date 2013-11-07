70ab78df3cd142df9e0b9efcbff78a4d830b4179
Leo C Schalkwyk <Leonard.Schalkwyk@kcl.ac.uk>
2013-11-07 19:45:51 +0000

    danes bug fix
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/wateRmelon@83034 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/dyebuy3.R
+++ R/dyebuy3.R
@@ -1,7 +1,7 @@
 danes <-
-function(mn, un, onetwo, fudge=100, ...){ 
+function(mn, un, onetwo, fudge=100, ret2=FALSE, ...){ 
 
   mnc <- dfsfit(mn, onetwo, ...)
   unc <- dfsfit(un, onetwo, ...)
-  nanes (mnc, unc, onetwo, fudge, ...)   
+  nanes (mnc, unc, onetwo, fudge, ret2, ...)   
 }
