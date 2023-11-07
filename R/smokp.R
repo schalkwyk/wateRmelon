f0198c0fdec5f73fff6f95bccd0f83e960c5575c
lschal <lschal@essex.ac.uk>
2023-11-07 09:58:30 +0000

    smokp merge, resave data files, datalist

--- R/smokp.R
+++ R/smokp.R
@@ -1,68 +1,92 @@
-smokp <- function(betas, method, sst=NULL){
-  load('smokp_cpgs.Rda')
+smokp <- function(betas, method='SSt', sst=NULL){
+  data("smokp_cpgs")
   if(method == 'SSt'){
     intercept <- lapply(smokp_cpgs[c('Never','Former','Current')], function(x){x['(Intercept)']})
-    coefs <- lapply(smokp_cpgs[c('Never','Former','Current')], function(x){
-      miss <- names(x) %in% rownames(na.omit(betas))
-      t(as.matrix(x[miss]))})
-    data <- lapply(coefs, function(x){betas[match(colnames(x), rownames(betas)),]})
-  } else {
+    coefs <- lapply(smokp_cpgs[c('Never','Former','Current')], function(x){nonmiss <- x[names(x) %in% rownames(na.omit(betas))]})
+    data <- lapply(coefs, function(x){betas[match(names(x), rownames(betas)), , drop = F]})
+  } else if(method %in% c('Packyears','Cessation')){
     intercept <- smokp_cpgs[[method]]['(Intercept)']
-    miss <- names(smokp_cpgs[[method]]) %in% rownames(na.omit(betas))
-    coefs <- t(as.matrix(smokp_cpgs[[method]][miss]))
-    data <- betas[match(colnames(coefs), rownames(betas)),]
+    coefs <- smokp_cpgs[[method]][names(smokp_cpgs[[method]]) %in% rownames(na.omit(betas))]
+    data <- betas[match(names(coefs), rownames(betas)), , drop = F]
+  } else if(method == 'Langdon'){
+    ilmnids <- split(smokp_cpgs$Langdon[,c('IlmnID','Effect')],smokp_cpgs$Langdon$Classes)
+    ilmnids <- lapply(ilmnids, function(x){setNames(x$Effect, x$IlmnID)})
+    coefs <- lapply(ilmnids, function(x){nonmiss <- x[names(x) %in% rownames(na.omit(betas))]})
+    data <- lapply(coefs, function(x){betas[match(names(x), rownames(betas)), , drop = F]})
+  } else {
+    coefs <- smokp_cpgs[[method]][names(smokp_cpgs[[method]]) %in% rownames(na.omit(betas))]
+    data <- betas[match(names(coefs), rownames(betas)), , drop = F]
   }
-  methods <- data.frame(method_sub=c('SSt',rep('smokp',2), rep('Methylation_Score',3),rep('Smoking_Index',4),'Zhang','Sugden'),
-                        row.names=c('SSt','Packyears','Cessation','McCartney','Christiansen','Odintsova',
-                                    'Teschendorff','Yu','Gao','Yang','Zhang','Sugden')
-                        , stringsAsFactors = F)
-  method_sub <- methods[method,'method_sub']
-  out <- switch(method_sub,
-                'SSt' = {
-                  log_odds <- list()
-                  for(i in c('Never','Former','Current')){
-                    log_odds[[i]] <- as.vector(coefs[[i]] %*% data[[i]] + intercept[[i]])
-                  } 
-                  odds <- sapply(log_odds, exp)
-                  log_odds <- data.frame(do.call(cbind,log_odds))
-                  sum_odds <- rowSums(odds)
-                  probs <- data.frame(apply(odds, 2, function(x){x/sum_odds}))
-                  out <- data.frame(as.character(factor(max.col(probs), levels=c(1:3), labels=c('Never','Former','Current'))))
-                },
-                'smokp' = {
-                  out <- data.frame(t(coefs %*% data + intercept))
-                },
-                'Methylation_Score' = {
-                  out <- data.frame(t(coefs %*% data))
-                },
-                'Smoking_Index' = {
-                  nevdat <- data[,match(names(sst[sst %in% 'Never']), colnames(data))]
-                  curdat <- data[,match(names(sst[sst %in% 'Current']), colnames(data))]
-                  nev_means <- apply(nevdat, 1, mean)
-                  cur_means <- apply(curdat, 1, mean)
-                  weights <- cur_means-nev_means
-                  weights <- ifelse(weights < 0, -1, 1)
-                  weights <- weights[colnames(coefs)]
-                  sds <- apply(nevdat, 1, sd)
-                  out <- apply(data, 2, function(x){x - nev_means / sds})
-                  out <- colSums(weights * out)
-                  out <- data.frame(1/length(weights) * out)
-                },
-                'Zhang' = {
-                  data <- t(as.matrix(data)) #only 1 Zhang CpG in beta matrix
-                  rownames(data) <- colnames(coefs)
-                  lowest_quartile <- setNames(apply(data,1,quantile)[2,],colnames(coefs))
-                  out <- list()
-                  for(i in colnames(coefs)){
-                    out[[i]] <- ifelse(data[i,] < lowest_quartile[i], 1, 0)
-                  }
-                  out <- data.frame(Reduce(`+`, out))
-                },
-                'Sugden' = {
-                  data <- data*100
-                  out <- apply(data, 2, function(x){x*coefs})
-                  out <- data.frame(colMeans(out))
-                })
-  colnames(out) <- method
-  return(out)
+  methods <- c('AHRR' = 'ahrr', 'SSt'='sst','Packyears'='smokp','Cessation'='smokp',
+               'McCartney'='methylation_score','Maas'='methylation_score',
+               'Teschendorff'='smoking_index','Yu'='smoking_index','Gao'='smoking_index','Yang'='smoking_index',
+               'Sugden'='sugden','Langdon'='langdon','Wen'='wen','All'='all')
+  method_sub <- methods[method]
+  smoking <- switch(method_sub,
+                    'ahrr' = {
+                      out <- data.frame(data['cg05575921',])
+                      colnames(out) <- method
+                      out
+                    },
+                    'sst' = {
+                      log_odds <- list(
+                        Never = as.vector(coefs[['Never']] %*% data[['Never']] + intercept[['Never']]),
+                        Former = as.vector(coefs[['Former']] %*% data[['Former']] + intercept[['Former']]),
+                        Current = as.vector(coefs[['Current']] %*% data[['Current']] + intercept[['Current']])
+                      )
+                      odds <- sapply(log_odds, exp)
+                      probs <- apply(odds, 2, function(x){x/rowSums(odds)})
+                      out <- data.frame(probs, 'SSt' = as.character(factor(max.col(probs), levels=c(1:3), labels=c('Never','Former','Current'))))
+                      rownames(out) <- colnames(data$Current)
+                      out
+                    },
+                    'smokp' = {
+                      out <- data.frame(t(coefs %*% data + intercept))
+                      colnames(out) <- method
+                      out
+                    },
+                    'methylation_score' = {
+                      out <- data.frame(t(coefs %*% data)) #in matrix multiplication columns of the left matrix = number of rows of the right
+                      colnames(out) <- method
+                      out
+                    },
+                    'smoking_index' = {
+                      dataspl <- list(Current = data[,names(sst[sst %in% 'Current'])],
+                                      Never = data[,names(sst[sst %in% 'Never'])])
+                      means <- lapply(dataspl, function(x){apply(x, 1, mean)})
+                      weights <- ifelse((means$Current-means$Never) < 0, -1, 1)
+                      sds <- apply(dataspl$Never, 1, sd)
+                      diff <- apply(data, 2, function(x){x - means$Never / sds})
+                      out <- data.frame(1/length(weights) * colSums(weights * diff))
+                      colnames(out) <- method
+                      out
+                    },
+                    'sugden' = {
+                      out <- apply(data*100, 2, function(x){x*coefs})
+                      out <- data.frame(colMeans(out))
+                      colnames(out) <- method
+                      out
+                    },
+                    'langdon' = {
+                      evernev <- as.numeric(coefs$'Ever vs never' %*% data$'Ever vs never')
+                      curform <- as.numeric(coefs$'Current vs former' %*% data$'Current vs former')
+                      out <- cbind(evernev,curform)
+                      colnames(out) <- paste0(method, '_', c('ever_never','current_former'))
+                      rownames(out) <- colnames(data$'Ever vs never')
+                      out
+                    },
+                    'wen' = {
+                      out <- data.frame(1/(1 + exp(-(10.621-10.005*data['cg05575921',] - 8.770*data['cg01940273',]))))
+                      colnames(out) <- method
+                      out
+                    },
+                    'all' = {
+                      allmethods <- c('SSt','Packyears','Cessation','McCartney','Maas','Teschendorff','Yu','Gao','Yang','Sugden','Langdon','Wen')
+                      out <- lapply(allmethods, function(x, betas, sst){
+                        smokp(betas = betas, sst = sst, method = x)
+                      }, betas=betas, sst=sst)
+                      out <- data.frame(do.call('cbind', out))
+                    out
+                    })
+  return(smoking)
 }
